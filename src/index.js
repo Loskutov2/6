84c1d912ce650af8a92bd136b4f402d7a018a097
elka <112241103+Loskutov2@users.noreply.github.com>
2023-07-28 19:08:03 +0300

    as

--- src/index.js
+++ src/index.js
@@ -4,3 +4,17 @@ const gallery = document.querySelector('.js-gallery')
 galleryItemTpl(countries)
 
 gallery.innerHTML = galleryItemTpl(countries)
+
+document.querySelector('#btn1').addEventListener('click', showOnly)
+document.querySelector('#btn2').addEventListener('click', showOnly)
+document.querySelector('#btn3').addEventListener('click', showOnly)
+document.querySelector('#btn4').addEventListener('click', showOnly)
+document.getElementById("btn4").addEventListener("click", showOnly);
+
+
+function showOnly(e) {
+    const Itms = document.querySelectorAll('.gallery__item')
+    const specialItms = document.querySelectorAll(e.target.dataset.show)
+    Itms.forEach(Itm=>{Itm.classList.add('hidden')})
+    specialItms.forEach(specialItm=>{specialItm.classList.remove('hidden')})
+}
